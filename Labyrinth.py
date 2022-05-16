a1197987deae21b31b7a9fc9b3d97d2a877f8889
y2bad <75477334+y2bad@users.noreply.github.com>
2022-05-16 20:42:18 +0200

    Add files via upload

--- Labyrinth.py
+++ Labyrinth.py
@@ -1,53 +1,75 @@
 import turtle
 
 
-def print_play_field() -> None:
+def update_screen():
+    global player_position, player_health, found_walls
+    turtle.clear()
+
     for i in range(10**2):
         turtle.penup()
         turtle.goto(i//10*30-30*5, i % 10*30-30*5)
         turtle.pendown()
+
         for j in range(4):
             turtle.fd(30)
             turtle.lt(90)
 
-
-def show_walls() -> None:
     for wall in found_walls:
         turtle.penup()
         turtle.goto(wall[0]*30-30*5, -wall[1]*30+30*4)
         turtle.pendown()
+
         turtle.fillcolor("black")
         turtle.begin_fill()
+
         for j in range(4):
             turtle.fd(30)
             turtle.lt(90)
+
         turtle.end_fill()
 
-
-def show_player(pos) -> None:
     turtle.penup()
-    turtle.goto(pos[0]*30-30*5+15, -pos[1]*30+30*4+15)
+    turtle.goto(player_position[0]*30-30*5+15, -player_position[1]*30+30*4+15)
     turtle.dot(15)
 
+    turtle.goto(-100, 160)
+    turtle.write(f'Verbleibende Leben: {player_health}', font=(
+        "Verdana", 15, "normal"))
 
-def click_regsitration(x, y) -> None:
-    global player_health, player_position
-    pos = [int(x//30+5), int((y//30-4)*-1)]
-    if pos[0] in [i for i in range(10)] and pos[1] in [i for i in range(10)] and \
-            pos[0] <= player_position[0]+1 and pos[0] >= player_position[0]-1 and \
-             pos[1] <= player_position[1]+1 and pos[1] >= player_position[1]-1:
+    turtle.goto(125, -145)
+    turtle.write(f'Ziel', font=(
+        "Verdana", 10, "normal"))
 
-        if play_field[pos[1]][pos[0]] == 0:
-            player_position = pos
+    turtle.update()
+
+
+def click_regsitration(x, y):
+    global game_field, player_position, player_health, found_walls
+    new_player_position = [int(x//30+5), int((y//30-4)*-1)]
+
+    move_allowed = (0 <= new_player_position[0] < 10 and 0 <= new_player_position[1] < 10 and
+                    new_player_position[0] == player_position[0]+1 and new_player_position[1] == player_position[1] or
+                    new_player_position[0] == player_position[0]-1 and new_player_position[1] == player_position[1] or
+                    new_player_position[0] == player_position[0] and new_player_position[1] == player_position[1]+1 or
+                    new_player_position[0] == player_position[0] and new_player_position[1] == player_position[1]-1)
+
+    if move_allowed:
+        if game_field[new_player_position[1]][new_player_position[0]] == 0:
+            player_position = new_player_position
+
         else:
             player_health -= 1
-            found_walls.append(pos)
+            found_walls.append(new_player_position)
+
+        update_screen()
 
         if player_position == [9, 9]:
             turtle.penup()
             turtle.goto(-50, 200)
             turtle.write(f'Gewonnen', font=(
                 "Verdana", 15, "normal"))
+
+            turtle.update()
             turtle.exitonclick()
 
         if player_health < 1:
@@ -55,23 +77,12 @@ def click_regsitration(x, y) -> None:
             turtle.goto(-40, 200)
             turtle.write(f'Verloren', font=(
                 "Verdana", 15, "normal"))
+
+            turtle.update()
             turtle.exitonclick()
 
-        turtle.clear()
-        print_play_field()
-        show_walls()
-        show_player(player_position)
-        turtle.penup()
-        turtle.goto(-100, 160)
-        turtle.write(f'Verbleibende Leben: {player_health}', font=(
-            "Verdana", 15, "normal"))
-        turtle.goto(125, -145)
-        turtle.write(f'Ziel', font=(
-            "Verdana", 10, "normal"))
-        turtle.update()
 
-
-play_field = [[0, 1, 0, 0, 0, 1, 0, 0, 0, 0],
+game_field = [[0, 1, 0, 0, 0, 1, 0, 0, 0, 0],
               [0, 0, 0, 1, 1, 1, 0, 1, 1, 0],
               [0, 1, 0, 1, 0, 0, 0, 0, 1, 0],
               [0, 1, 0, 0, 0, 1, 0, 1, 1, 0],
@@ -83,24 +94,15 @@ play_field = [[0, 1, 0, 0, 0, 1, 0, 0, 0, 0],
               [0, 1, 0, 0, 1, 0, 0, 0, 1, 0]]
 
 player_position = [0, 0]
-player_health = 5
+player_health = 300
 found_walls = []
+
+screen = turtle.Screen()
+screen.onclick(click_regsitration)
 
 turtle.tracer(0, 0)
 turtle.hideturtle()
 
-screen = turtle.Screen()
-screen.onclick(fun=click_regsitration)
+update_screen()
 
-print_play_field()
-show_player(player_position)
-turtle.penup()
-turtle.goto(-100, 160)
-turtle.write(f'Verbleibende Leben: {player_health}', font=(
-    "Verdana", 15, "normal"))
-turtle.goto(125, -145)
-turtle.write(f'Ziel', font=(
-    "Verdana", 10, "normal"))
-
-turtle.update()
 turtle.mainloop()
